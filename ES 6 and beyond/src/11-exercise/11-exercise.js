8fd5b0574c09c4f166c5e308f9bae93ddeea47d2
indarit <109621400+indarit@users.noreply.github.com>
2022-10-08 12:59:01 +0200

    ejecicios 11 listo

--- ES 6 and beyond/src/11-exercise/11-exercise.js
+++ ES 6 and beyond/src/11-exercise/11-exercise.js
@@ -16,7 +16,10 @@
  */
 
 // Finish the code of the function
-function exercise11() {}
+function exercise11(array, entry) {
+    const newArray = array.find((element) => element === entry);
 
+    return newArray;
+}
 // Don’t change the code bellow this line
 export default exercise11;
