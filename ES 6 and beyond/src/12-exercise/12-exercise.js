ae6ccd4a2389fface44ab41c800eda05f5f5419f
indarit <109621400+indarit@users.noreply.github.com>
2022-10-08 13:08:07 +0200

    ejecicios 12 listo

--- ES 6 and beyond/src/12-exercise/12-exercise.js
+++ ES 6 and beyond/src/12-exercise/12-exercise.js
@@ -16,7 +16,9 @@
  */
 
 // Finish the code of the function
-function exercise12() {}
-
+function exercise12(array, entry) {
+    const newArray = array.includes(entry);
+    return newArray;
+}
 // Don’t change the code bellow this line
 export default exercise12;
