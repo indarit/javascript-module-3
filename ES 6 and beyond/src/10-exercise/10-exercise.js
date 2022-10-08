7bc18a0709abb9e2dbc2d802805385b35262db93
indarit <109621400+indarit@users.noreply.github.com>
2022-10-08 12:43:11 +0200

    ejecicios 10 listo

--- ES 6 and beyond/src/10-exercise/10-exercise.js
+++ ES 6 and beyond/src/10-exercise/10-exercise.js
@@ -11,6 +11,7 @@
 // Finish the code of the function
 function exercise10() {
     const message = "hello-world";
+    let messageArray = Array.from(message);
 }
 
 // Don’t change the code bellow this line
